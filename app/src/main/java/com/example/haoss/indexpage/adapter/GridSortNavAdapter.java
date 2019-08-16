b5e86651b120a4568f2b2ed6c19ebf8274f9456f
chentt521520 <15129858157@163.com>
2019-08-16 16:12:02 +0800

    修改

--- app/src/main/java/com/example/haoss/indexpage/adapter/GridSortNavAdapter.java
+++ app/src/main/java/com/example/haoss/indexpage/adapter/GridSortNavAdapter.java
@@ -8,7 +8,6 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.example.applibrary.entity.Nav;
 import com.example.applibrary.utils.DensityUtil;
 import com.example.applibrary.utils.ImageUtils;
